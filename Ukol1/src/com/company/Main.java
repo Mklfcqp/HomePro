b3d17f52a1bff2c08f87f85da75519f59527a557
Mklfcqp <tomas.senti@seznam.cz>
2021-10-19 20:11:33 +0200

    Version 1.0.0

---
+++ Ukol1/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Petr Novák";
+        int numberOfpurchases = 10;
+        String city = "Praha";
+        double consumption = 6.5;
+        double cablesLength = 1000.00;
+        LocalDate birthDate = LocalDate.of(1989,10,6);
+        String SPZ = "7C2 5025";
+        String ipAdress = "192.168.50.14";
+    }
+}
