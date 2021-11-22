7ed50510a96ae07524a196914c2a573268f479e9
Mklfcqp <tomas.senti@seznam.cz>
2021-11-22 20:22:27 +0100

    Version 2.0

--- Ukol5/src/com/company/Main.java
+++ Ukol5/src/com/company/Main.java
@@ -5,117 +5,95 @@ import java.util.List;
 
 public class Main {
 
-    //region Demo
-    /**
-     * Ukázka použítí metody {@link Support#safeReadInt()}
-     *
-     * Tato metoda použije metodu {@link Support#safeReadInt() k načtení
-     * jednoho čísla od uživatele a vypsání tohoto čísla na výstup.
-     *
-     * Podobně můžeš používat metodu {@link Support#safeReadInt()}
-     * i v dalších metodách.
-     */
     public static void readOneIntFromInputAndPrintIt() {
         int input = Support.safeReadInt();
         System.out.println("You entered: "+input);
     }
-    //endregion
 
-    //region Úlohy na cykly
-    /**
-     * Čte čísla, která zadává uživatel a vypisuje je na výstup.
-     * Metoda pokračuje ve čtení opakovaně tak dlouho, dokud uživatel zadává kladná čísla či nulu.
-     *
-     * Jakmile uživatel zadá záporné číslo, metoda skončí.
-     */
     public static void readIntsFromInputAndPrintItUntilNegative() {
+        int input;
+        do {
+            input = Support.safeReadInt();
+            System.out.println("You entered: " + input);
+        } while (input >= 0);
     }
 
-    /**
-     * Čte čísla ze vstupu tak dlouho, dokud uživatel nezadá záporné číslo.
-     * Jako návratovou hodnotu vrátí součet zadaných čísel (záporné číslo by nemělo být zahrnuto).
-     *
-     * @return Součet všech zadaných čísel (záporné číslo ukončující vstup není zahrnuto).
-     */
     public static int sumAllInputUntilNegative() {
-        return 0;
+        int sum = 0, input = 0;
+        while (input >= 0) {
+            sum += input;
+            input = Support.safeReadInt();
+        }
+        return sum;
     }
 
-    /**
-     * Čte čísla ze vstupu tak dlouho, dokud uživatel nezadá záporné číslo.
-     * Všechna načtená čísla uloží do listu, který vrátí jako návratovou hodnotu.
-     *
-     * @return List načtených hodnot (záporná hodnota není zahrnuta v listu)
-     */
     public static List storeAllInputInArrayListUntilNegative() {
-        return null;
+        int input = 0;
+        List<Integer> result = new ArrayList<>();
+        while (input >= 0) {
+            input = Support.safeReadInt();
+            result.add(input);
+        }
+        result.remove(result.size()-1);
+        return result;
     }
 
-    /**
-     * Dostane jako parametr list čísel. Vypíše čísla na obrazovku. Každé číslo vypiš na samostatný řádek.
-     * @param list List čísel, která se mají vypsat.
-     */
     public static void printAllIntegersFromList(List<Integer> list) {
+        for (Integer i : list) {
+            System.out.println(i);
+        }
     }
 
-    /**
-     * Sečte hodnoty z listu a vrátí výsledek jako návratovou hodnotu.
-     * @param list List čísel, která chceme sečíst.
-     * @return Součet všech čísel v listu.
-     */
     public static Integer sumAllIntegersFromList(List<Integer> list) {
-        return null;
+        Integer result = 0;
+         for (Integer i : list) {
+            result += i;
+        }
+        return result;
     }
-    //endregion
 
-    //region Podmínky
-    /**
-     * Vypíše čísla ze seznamu, která jsou menší než zadaný limit.
-     * @param list Seznam čísel, která máme zkontrolovat.
-     * @param limit Limit - vypsána budou pouze čísla z listu, která jsou menší než limit
-     */
     public static void printIntegersUnderLimit(List<Integer> list, int limit) {
+        for (Integer i : list) {
+            if (i < limit) System.out.println(i);
+        }
     }
 
-    /**
-     * Vypíše čísla ze zadaného listu.
-     * Při výpisu ale nahradí:
-     * - číslo 1 slovem "one",
-     * - číslo 2 slovem "two",
-     * - číslo 3 slovem "three"
-     * Ostatní čísla vypíše jako číselnou hodnotu.
-     * Například pokud dostane jako parametr seznam [5, 3, 1, 48, 2],
-     * vypíše: 5, three, one, 48, two.
-     * @param list Seznam čísel, která se mají vypsat.
-     */
     public static void printIntegersWithReplace(List<Integer> list) {
+        for (Integer i : list) {
+            switch(i) {
+                case 1: System.out.println("one"); break;
+                case 2: System.out.println("two"); break;
+                case 3: System.out.println("three"); break;
+                default: System.out.println(i); break;
+            }
+        }
+
     }
-    //endregion
 
     public static void main(String[] args) {
-        // Příklad:
+
         System.out.println("--- Example - read and print one integer ---");
         readOneIntFromInputAndPrintIt();
-        // ---
+
         System.out.println("--- Task 1 - read and print integers until negative ---");
         readIntsFromInputAndPrintItUntilNegative();
-        // ---
+
         System.out.println("--- Task 2 - sum all integers until negative ---");
         System.out.println("Total sum: " + sumAllInputUntilNegative());;
-        // ---
+
         System.out.println("--- Task 3 - read integers to list until negative ---");
         List<Integer> list = storeAllInputInArrayListUntilNegative();
         System.out.println(list);
-        // ---
+
         System.out.println("--- Task 4 - print all integers from list ---");
         printAllIntegersFromList(list);
-        // ---
+
         System.out.println("--- Task 5 - sum all integers in list ---");
         System.out.println("Total sum: " + sumAllIntegersFromList(list));
-        // ---
+
         System.out.println("--- Task 6 - print integers under 5 from list ---");
         printIntegersUnderLimit(list, 5);
-        // ---
+
         System.out.println("--- Task 7 - print integers - replace: 1 => \"one\", 2 => \"two\", 3 => \"three\" ---");
         printIntegersWithReplace(list);
     }
--- Ukol5/src/com/company/Support.java
+++ Ukol5/src/com/company/Support.java
@@ -14,11 +14,6 @@ public class Support {
 
     public static int INVALID_INPUT = -1;
 
-    /**
-     * Read one integer. If given input cannot be converted to integer,
-     * prints error and return -1;
-     * @return Integer read or -1 if invalid input entered
-     */
     public static int safeReadInt() {
         int result = INVALID_INPUT;
         String inputText = getScanner().nextLine();
